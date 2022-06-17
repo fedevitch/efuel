68f54d92760f685f12603541a0a44cdbf8ec3d6d
lyubomyr <lyubomyr.fedevitch@gmail.com>
2022-06-17 21:40:53 +0300

    refactor

--- efuel/components/requests.ts
+++ efuel/components/requests.ts
@@ -24,69 +24,79 @@ const isInRange = (range:number, point1: Coordinates, point2: Coordinates):boole
 }
 
 export const fetchOkko = async (params: FetchParams):Promise<Array<FuelStation>> => {
-
-    const response = okko as OkkoStation // request to okko
-
     const fuel_stations:Array<FuelStation> = []
-    response.collection
-        .filter(station => {
-            return isInRange(params.range, 
-                params.location, 
-                {lat: station.attributes.coordinates.lat, lon: station.attributes.coordinates.lng} as Coordinates)
-        })
-        .forEach(station => {
-            const push = () => {                
-                fuel_stations.push(new FuelStation(station.attributes))
-            }
-            switch(params.fuelType){                
-                case FuelTypes.A95:
-                    if(station.attributes.a95_evro_tip_oplati || station.attributes.pulls95_tip_oplati){
-                        push()                        
-                    }
-                    break
-                case FuelTypes.DIESEL:
-                    if(station.attributes.dp_evro_tip_oplati || station.attributes.pullsdiesel_tip_oplati) {
-                        push()                        
-                    }
-                    break 
-                case FuelTypes.A92:
-                    if(station.attributes.a92_evro_tip_oplati){
-                        push()
-                    }
-                    break
-                case FuelTypes.LPG:
-                    if(station.attributes.gas_tip_oplati){
-                        push()
-                    }
-                    break    
-                default:
-                    break       
 
-            }
-        })
+    try {
+        const response = okko as OkkoStation // request to okko
+
+        response.collection
+            .filter(station => {
+                return isInRange(params.range, 
+                    params.location, 
+                    {lat: station.attributes.coordinates.lat, lon: station.attributes.coordinates.lng} as Coordinates)
+            })
+            .forEach(station => {
+                const push = () => {                
+                    fuel_stations.push(new FuelStation(station.attributes))
+                }
+                switch(params.fuelType){                
+                    case FuelTypes.A95:
+                        if(station.attributes.a95_evro_tip_oplati || station.attributes.pulls95_tip_oplati){
+                            push()                        
+                        }
+                        break
+                    case FuelTypes.DIESEL:
+                        if(station.attributes.dp_evro_tip_oplati || station.attributes.pullsdiesel_tip_oplati) {
+                            push()                        
+                        }
+                        break 
+                    case FuelTypes.A92:
+                        if(station.attributes.a92_evro_tip_oplati){
+                            push()
+                        }
+                        break
+                    case FuelTypes.LPG:
+                        if(station.attributes.gas_tip_oplati){
+                            push()
+                        }
+                        break    
+                    default:
+                        break       
+
+                }
+            })
+    } catch {
+        console.error('Error while fetching data from OKKO')
+    }
 
     return fuel_stations
 }
 
 export const fetchWog = async(params: FetchParams):Promise<Array<FuelStation>> => {
-    const response = wog as WogStation // request to wog
     const fuel_stations:Array<FuelStation> = []
 
-    response.data.stations
-            .filter(station => 
-                isInRange(params.range, params.location, { lat: station.coordinates.latitude, lon: station.coordinates.longitude}))
-            .forEach(station => {
-                // another async request for availability here 
-                fuel_stations.push(new FuelStation(station))
-            })
-
+    try {
+        const response = wog as WogStation // request to wog
+        
+        response.data.stations
+                .filter(station => 
+                    isInRange(params.range, params.location, { lat: station.coordinates.latitude, lon: station.coordinates.longitude}))
+                .forEach(station => {
+                    // another async request for availability here 
+                    fuel_stations.push(new FuelStation(station))
+                })
+    } catch {
+        console.error('Error while fetching data from WOG')
+    }
     return fuel_stations
 }
 
 export const fetchSocar = async(params: FetchParams):Promise<Array<FuelStation>> => {
-    const response = socar as SocarStations
     const fuel_stations:Array<FuelStation> = []
 
+    try {
+    const response = socar as SocarStations
+    
     response.data
             .filter(station => 
                 isInRange(params.range, params.location, { lat: station.attributes.marker.lat, lon: station.attributes.marker.lng }))
@@ -115,43 +125,51 @@ export const fetchSocar = async(params: FetchParams):Promise<Array<FuelStation>>
     
                 }
             })
+        } catch {
+            console.error('Error while fetching data from Socar')
+        }
             
     return fuel_stations        
 
 }
 
 export const fetchUkrnafta = async(params: FetchParams):Promise<Array<FuelStation>> => {
-    const response = ukrnafta as Array<UkrnaftaStation>
     const fuel_stations:Array<FuelStation> = []
 
-    response.filter(station => isInRange(params.range, params.location, {
-        lat: Number.parseFloat(station.lat), lon: Number.parseFloat(station.lon)
-    }))
-    .forEach(station => {                
-        const push = () => {               
-            fuel_stations.push(new FuelStation(station))
-        }
-        switch(params.fuelType){                
-            case FuelTypes.A95:
-                if(station.a95 !== "0.00" || station.a95e !== "0.00"){
-                    push()                        
-                }
-                break
-            case FuelTypes.DIESEL:
-                if(station.dt !== "0.00" || station.dte !== "0.00" ){
-                    push()                        
-                }
-                break 
-            case FuelTypes.LPG:
-                if(station.gas !== "0.00" ){
-                    push()                        
-                }
-                break 
-            default:
-                break       
+    try {
+        const response = ukrnafta as Array<UkrnaftaStation>
+        
+        response.filter(station => isInRange(params.range, params.location, {
+            lat: Number.parseFloat(station.lat), lon: Number.parseFloat(station.lon)
+        }))
+        .forEach(station => {                
+            const push = () => {               
+                fuel_stations.push(new FuelStation(station))
+            }
+            switch(params.fuelType){                
+                case FuelTypes.A95:
+                    if(station.a95 !== "0.00" || station.a95e !== "0.00"){
+                        push()                        
+                    }
+                    break
+                case FuelTypes.DIESEL:
+                    if(station.dt !== "0.00" || station.dte !== "0.00" ){
+                        push()                        
+                    }
+                    break 
+                case FuelTypes.LPG:
+                    if(station.gas !== "0.00" ){
+                        push()                        
+                    }
+                    break 
+                default:
+                    break       
 
-        }
-    })
+            }
+        })
+    } catch {
+        console.error('Error while fetching data from Ukrnafta')
+    }
 
     return fuel_stations
 }
