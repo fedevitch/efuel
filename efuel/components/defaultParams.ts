b4145e386266d694ab5bd89b32a1a0464ddc66f5
lyubomyr <lyubomyr.fedevitch@gmail.com>
2022-06-22 17:03:12 +0300

    ДЕСЯТЬ

--- efuel/components/defaultParams.ts
+++ efuel/components/defaultParams.ts
@@ -1,8 +1,8 @@
 import { Coordinates, FuelTypes } from "../models/fuelStation";
 
 export default {
-    RANGE_R: 0.044988888,
-    RANGE_KM: 5,
+    RANGE_R: 0.088888888,
+    RANGE_KM: 10,
     LOCATION: { lat: 49.840762, lon: 24.0291513 } as Coordinates,
     FUEL_TYPE: FuelTypes.A95
 }
