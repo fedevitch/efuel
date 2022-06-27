2f53e40a81bcc4faa8d614f3c7d7934bc1aa671b
lyubomyr <lyubomyr.fedevitch@gmail.com>
2022-06-27 19:23:43 +0300

    added PWA

--- efuel/next.config.js
+++ efuel/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')
+const nextConfig = withPWA({
   reactStrictMode: false,
-}
+  pwa: {
+    dest: 'public'
+  },
+})
 
 module.exports = nextConfig
