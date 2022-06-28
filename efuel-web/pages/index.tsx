57bb9b9638cb8dba5dfec5b6f646f28a5b94365c
lyubomyr <lyubomyr.fedevitch@gmail.com>
2022-06-28 18:27:30 +0300

    added site icon

--- efuel-web/pages/index.tsx
+++ efuel-web/pages/index.tsx
@@ -113,13 +113,13 @@ const PWAHeadMeta = (
     <meta name='msapplication-tap-highlight' content='no' />
     <meta name='theme-color' content='#000000' />
 
-    <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
-    <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
-    <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
-    <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' />
+    <link rel='apple-touch-icon' href='/efuel.png' />
+    <link rel='apple-touch-icon' sizes='152x152' href='/efuel.png' />
+    <link rel='apple-touch-icon' sizes='180x180' href='/efuel.png' />
+    <link rel='apple-touch-icon' sizes='167x167' href='/efuel.png' />
 
-    <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
-    <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+    <link rel='icon' type='image/png' sizes='32x32' href='/efuel.png' />
+    <link rel='icon' type='image/png' sizes='16x16' href='/efuel.png' />
     <link rel='manifest' href='/manifest.json' />
     <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
     <link rel='shortcut icon' href='/favicon.ico' />
@@ -128,6 +128,6 @@ const PWAHeadMeta = (
     <meta property='og:type' content='website' />
     <meta property='og:title' content='єПаливо' />
     <meta property='og:description' content='Перевірити наявність палива поблизу' />
-    <meta property='og:site_name' content='efuel' />
+    <meta property='og:site_name' content='єПаливо' />
   </Fragment>
 )
