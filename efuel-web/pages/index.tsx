f51cab3314371464026b6d1bddef8ece132794bf
lyubomyr <lyubomyr.fedevitch@gmail.com>
2022-07-04 15:52:17 +0300

    icons

--- efuel-web/pages/index.tsx
+++ efuel-web/pages/index.tsx
@@ -107,21 +107,19 @@ const PWAHeadMeta = (
     <meta name='apple-mobile-web-app-title' content='єПаливо' />
     <meta name='description' content='Швидко дізнатися яке паливо у наявності поблизу' />
     <meta name='format-detection' content='telephone=no' />
-    <meta name='mobile-web-app-capable' content='yes' />
-    <meta name='msapplication-config' content='/icons/browserconfig.xml' />
+    <meta name='mobile-web-app-capable' content='yes' />    
     <meta name='msapplication-TileColor' content='#2B5797' />
     <meta name='msapplication-tap-highlight' content='no' />
     <meta name='theme-color' content='#000000' />
 
-    <link rel='apple-touch-icon' href='/efuel.png' />
-    <link rel='apple-touch-icon' sizes='152x152' href='/efuel.png' />
-    <link rel='apple-touch-icon' sizes='180x180' href='/efuel.png' />
-    <link rel='apple-touch-icon' sizes='167x167' href='/efuel.png' />
+    <link rel='apple-touch-icon' href='/public/efuel.png' />
+    <link rel='apple-touch-icon' sizes='152x152' href='/efuel_152x152.png' />
+    <link rel='apple-touch-icon' sizes='180x180' href='/efuel_180x180.png' />
+    <link rel='apple-touch-icon' sizes='167x167' href='/efuel_167x167.png' />
 
-    <link rel='icon' type='image/png' sizes='32x32' href='/efuel.png' />
-    <link rel='icon' type='image/png' sizes='16x16' href='/efuel.png' />
-    <link rel='manifest' href='/manifest.json' />
-    <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+    <link rel='icon' type='image/png' sizes='32x32' href='/efuel_32x32.png' />
+    <link rel='icon' type='image/png' sizes='16x16' href='/efuel_16x16.png' />
+    <link rel='manifest' href='/manifest.json' />    
     <link rel='shortcut icon' href='/favicon.ico' />
     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
         
